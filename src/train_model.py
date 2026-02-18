cd81278e1a05639451df71da5266d47ec066aa8c
Eris Kanapari <eriskanapari9@gmail.com>
2026-02-18 17:16:55 -0500

    refactor: improve training script structure and evaluation output

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import joblib
 import os
+from pathlib import Path
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -11,7 +12,14 @@ from src.feature_extracion import extract_features
 
 
 def train():
-    df = pd.read_csv("C:\\Users\\erisk\\Desktop\\PythonProjects\\fake-news-detector\\data\\raw\\news.csv")
+    project_root = Path(__file__).resolve().parent.parent
+    file_news_path = next(project_root.rglob("news.csv"))
+
+    if file_news_path is None:
+        raise FileNotFoundError("File news.csv not found")
+
+
+    df = pd.read_csv(file_news_path)
     df['clean_text'] = df['text'].apply(clean_text)
     x, vectorizer = extract_features(df['clean_text'])
     y = df['label']
